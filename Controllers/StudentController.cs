47daf02e075a0afd4736b9f92fcddc906cd138e5
Ajay K <113184147+ajayfoo@users.noreply.github.com>
2024-05-09 15:10:17 +0530

    chore: add boilerplace code for ef

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRK.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class StudentController : ControllerBase { }
---
+++ Models/Student.cs
@@ -0,0 +1,16 @@
+using System.Data.SqlTypes;
+
+namespace CRK.Models;
+
+public class Student
+{
+    public SqlString FirstName { get; set; }
+    public SqlString? MiddleName { get; set; }
+    public SqlString LastName { get; set; }
+    public SqlDouble CetPercentile { get; set; }
+    public SqlDouble HscPercentage { get; set; }
+    public SqlDouble SscPercentage { get; set; }
+    public SqlDateTime Dob { get; set; }
+    public SqlDateTime YearOfAdmission { get; set; }
+    public readonly SqlGuid Id = Guid.NewGuid();
+}
